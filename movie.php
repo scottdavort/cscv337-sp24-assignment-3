3fba2dd0a9b2d2776ed93941d39b9cf64222d3a3
scottdavort <58272461+scottdavort@users.noreply.github.com>
2024-02-29 18:31:47 -0700

    test

--- movie.php
+++ movie.php
@@ -69,7 +69,7 @@ if (is_dir($posterDirPath)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($title); ?> - Movie Review</title>
+    <title>THIS IS A TEST <?php echo htmlspecialchars($title); ?> - Movie Review</title>
     <link rel="stylesheet" href="./css/movie.css">
     <!-- Google Materials -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
